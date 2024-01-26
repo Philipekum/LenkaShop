cfa033beef5d5f0011c024fc5888ba3be65db3ae
Philipp <filipp.tsekhansky@yandex.ru>
2024-01-26 04:58:23 +0300

    inital base html, upgrade urls and static

--- main/views.py
+++ main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    context = {
+        'title': 'Главная страница',
+        'content': 'Some content'
+    }
+
+    return render(request, 'main/index.html', context)
