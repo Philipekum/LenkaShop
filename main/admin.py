5ae143ce200acd768328533590de13925967d26f
Philipp <filipp.tsekhansky@yandex.ru>
2024-11-01 03:08:35 +0300

    начал модифицировать index.html создал модели данных для блоков текста и контента

--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
-from django.urls.resolvers import URLPattern
-from .models import InfoPage
+from .models import InfoPage, MainPageContentBox, MainPageTextBox
 
 @admin.register(InfoPage)
 class InfoPageAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug')
     prepopulated_fields = {'slug': ('title',)}
     search_fields = ('title',)
+
+
+@admin.register(MainPageTextBox)
+class MainPageTextBoxAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+
+@admin.register(MainPageContentBox)
+class MainPageContentBoxAdmin(admin.ModelAdmin):
+    list_display = ('title',)
--- main/models.py
+++ main/models.py
@@ -1,5 +1,35 @@
 from django.db import models
 from django.urls import reverse
+from goods.models import Products
+
+
+class MainPageTextBox(models.Model):
+    title = models.CharField(max_length=150, blank=True, verbose_name='Заголовок')
+    text = models.TextField(verbose_name='Текст')
+
+    class Meta:
+        db_table = 'main_page_text_box'
+        verbose_name = 'Блок текста'
+        verbose_name_plural = 'Блоки текста'
+
+
+class MainPageContentBox(models.Model):
+    ALIGNMENT_CHOICES = [
+        ('left', 'Слева'),
+        ('right', 'Справа'),
+    ]
+
+    title = models.CharField(max_length=150, blank=True, verbose_name='Заголовок')
+    image1 = models.ImageField(upload_to='main_page_images', verbose_name='Большое фото 1')
+    product1 = models.ForeignKey(Products, on_delete=models.CASCADE, related_name='product_1', verbose_name='Продукт 1')
+    product2 = models.ForeignKey(Products, on_delete=models.CASCADE, related_name='product_2', verbose_name='Продукт 2')
+    alignment = models.CharField(max_length=5, choices=ALIGNMENT_CHOICES, default='left', verbose_name='Расположение')
+
+    class Meta:
+        db_table = 'main_page_content_box'
+        verbose_name = 'Блок контента'
+        verbose_name_plural = 'Блоки контента'
+
 
 class InfoPage(models.Model):
     title = models.CharField(max_length=150, verbose_name='Название страницы')
