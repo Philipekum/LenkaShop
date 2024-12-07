eeb941c09197e581b50e4dc82c6091da7a5adcd1
Philipp <filipp.tsekhansky@yandex.ru>
2024-12-07 08:21:54 +0300

    добавил STATICFILES_STORAGE

--- app/settings.py
+++ app/settings.py
@@ -148,6 +148,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
 if not DEBUG:
     STATIC_ROOT = BASE_DIR / 'staticfiles'
     STATIC_URL = 'staticfiles/'
