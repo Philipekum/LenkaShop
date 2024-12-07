e85d9b3659efeb1b5ab67b776762b709d6475390
Philipp <filipp.tsekhansky@yandex.ru>
2024-12-07 07:43:19 +0300

    добавил фильтрацию по категориям, добавил дня нее стили

--- goods/views.py
+++ goods/views.py
@@ -1,14 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from goods.models import Products
+from goods.models import Products, Categories
 
 
 def catalog(request):
+    category_slug = request.GET.get('category')
     products = Products.objects.all()
+
+    if category_slug:
+        category = get_object_or_404(Categories, slug=category_slug)
+        products = products.filter(category=category)
+    
+    else:
+        category = None
+    
+    categories = Categories.objects.all()
 
     context = {
         'title': 'Каталог',
         'products': products,
+        'categories': categories,
+        'selected_category': category,
     }
     
     return render(request, 'goods/catalog.html', context)
