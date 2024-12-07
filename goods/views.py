837e8d0a98a1417781793f938783255ee71260e0
Philipp <filipp.tsekhansky@yandex.ru>
2024-12-07 07:13:08 +0300

    убрал пагинацию

--- goods/views.py
+++ goods/views.py
@@ -1,23 +1,18 @@
 from django.shortcuts import render
-from django.core.paginator import Paginator
 
 from goods.models import Products
 
 
 def catalog(request):
-    page = int(request.GET.get('page', 1))
-
     products = Products.objects.all()
-
-    paginator = Paginator(products, 7)
-    current_page = paginator.page(page)
 
     context = {
         'title': 'Каталог',
-        'products': current_page,
+        'products': products,
     }
     
     return render(request, 'goods/catalog.html', context)
+
 
 def product(request, product_slug):
     product = Products.objects.get(slug=product_slug)
