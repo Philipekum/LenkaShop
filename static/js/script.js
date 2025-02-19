4143afc20c00d5ec7c0a6e9b88176c1d9ec3c97f
Philipp <filipp.tsekhansky@yandex.ru>
2025-02-20 00:21:22 +0300

    исправил ошибку с формой оплаты

--- static/js/script.js
+++ static/js/script.js
@@ -29,19 +29,21 @@ if (selectSingle_title) {
 
 // Редирект на платежную страницу
 const form = document.getElementById('payment-form');
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    fetch(form.action, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'X-CSRFToken': document.querySelector('[name=csrfmiddlewaretoken]').value
-        }
-    }).then(response => response.json())
-      .then(data => {
-          window.location.href = data.redirect_url;
-      });
-});
+if (form) {
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        fetch(form.action, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': document.querySelector('[name=csrfmiddlewaretoken]').value
+            }
+        }).then(response => response.json())
+          .then(data => {
+              window.location.href = data.redirect_url;
+          });
+    });
+}
 
 //add2cart button	
 // const add2cart = document.querySelector('.add2cart_btn');
