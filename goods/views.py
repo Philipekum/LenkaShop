ad488b0227b2053f810448482d8c976ed1f23586
Philipekum <filipp.tsekhansky@yandex.ru>
2025-10-29 11:37:53 +0300

    basic load more button

--- goods/views.py
+++ goods/views.py
@@ -1,28 +1,36 @@
 from django.shortcuts import render, get_object_or_404
+from django.template.loader import render_to_string
+from django.http import HttpResponse
 
 from goods.models import Products, Categories
+
+
+BLOCK_LIMIT = 6
 
 
 def catalog(request):
     category_slug = request.GET.get('category')
     products = Products.objects.all()
-
+    
     if category_slug:
         category = get_object_or_404(Categories, slug=category_slug)
         products = products.filter(category=category)
-    
     else:
         category = None
-    
+
     categories = Categories.objects.all()
+    offset = 0
 
     context = {
         'title': 'Каталог',
-        'products': products,
+        'products': products[:BLOCK_LIMIT],
         'categories': categories,
         'selected_category': category,
+        'has_more': products.count() > BLOCK_LIMIT,
+        'limit': BLOCK_LIMIT,
+        'offset': offset + BLOCK_LIMIT, 
     }
-    
+
     return render(request, 'goods/catalog.html', context)
 
 
@@ -35,3 +43,26 @@ def product(request, product_slug):
     }
 
     return render(request, 'goods/product.html', context)
+
+
+def catalog_load_more(request):
+    category_slug = request.GET.get('category')
+    offset = int(request.GET.get('offset', 0))
+
+    products_qs = Products.objects.all()
+    if category_slug:
+        products_qs = products_qs.filter(category__slug=category_slug)
+
+    products = products_qs[offset:offset + BLOCK_LIMIT]
+    has_more = products_qs.count() > offset + BLOCK_LIMIT
+
+    html_products = render_to_string('goods/catalog_items.html', {'products': products})
+    html_button = ''
+
+    if has_more:
+        html_button = render_to_string('goods/load_more_button.html', {
+            'offset': offset + BLOCK_LIMIT,
+            'category_slug': category_slug
+        })
+
+    return HttpResponse(html_products + html_button)
