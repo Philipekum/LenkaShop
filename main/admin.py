a1c15116ee572e8586df8baaa28ff7051eb1450c
Philipekum <filipp.tsekhansky@yandex.ru>
2025-10-29 12:24:39 +0300

    deleted unused main page elements

--- main/admin.py
+++ main/admin.py
@@ -1,51 +1,5 @@
 from django.contrib import admin
 from .models import *
-from .forms import *
-
-@admin.register(InfoPage)
-class InfoPageAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'position')
-    prepopulated_fields = {'slug': ('title',)}
-    search_fields = ('title',)
-    list_editable = ('position',)
-    form = InfoPageForm
-
-
-class MainPageTextBoxInline(admin.TabularInline):
-    model = MainPageTextBox
-    form = MainPageTextBoxForm
-    extra = 1
-
-
-class MainPageContentBoxInline(admin.TabularInline):
-    model = MainPageContentBox
-    form = MainPageContentBoxForm
-    extra = 1
-
-
-class CarouselImageInline(admin.TabularInline):
-    model = CarouselImage
-    extra = 1
-    fields = ('image',)
-
-
-class CarouselBlockInline(admin.TabularInline):
-    model = CarouselBlock
-    extra = 1
-
-
-@admin.register(Carousel)
-class CarouselAdmin(admin.ModelAdmin):
-    list_display = ('title',)
-    inlines = [CarouselImageInline]
-
-
-@admin.register(MainPage)
-class MainPageAdmin(admin.ModelAdmin):
-    list_display = ('title', 'is_active')  
-    list_editable = ('is_active',)         
-    list_filter = ('is_active',) 
-    inlines = [MainPageTextBoxInline, MainPageContentBoxInline, CarouselBlockInline]
 
 
 @admin.register(ContactInfo)
--- main/templatetags/menu_tags.py
+++ main/templatetags/menu_tags.py
@@ -1,18 +1,8 @@
 from django import template
-from main.models import InfoPage, ContactInfo
+from main.models import ContactInfo
 
 
 register = template.Library()
-
-
-@register.simple_tag
-def header_links():
-    return InfoPage.objects.filter(position__in=[1, 2])
-
-
-@register.simple_tag
-def footer_links():
-    return InfoPage.objects.filter(position__in=[3, 4, 5, 6, 7, 8])
 
 
 @register.simple_tag
