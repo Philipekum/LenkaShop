5957a549395467600eab89c392977e1b9b4b941c
Philipekum <filipp.tsekhansky@yandex.ru>
2025-11-12 15:49:48 +0300

    bugfix

--- app/settings.py
+++ app/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG_STATUS', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 if DEBUG:
     ALLOWED_HOSTS.append('127.0.0.1')
