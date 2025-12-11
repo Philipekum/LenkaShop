a3c5a110ac5a9f0583ea90001e5bbd4a77fca434
Philipekum <filipp.tsekhansky@yandex.ru>
2025-12-11 23:57:06 +0300

    payments verbose added

--- payments/apps.py
+++ payments/apps.py
@@ -4,6 +4,7 @@ from django.apps import AppConfig
 class PaymentsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'payments'
+    verbose_name = 'Оплаты'
     
     def ready(self):
         from payments.yookassa_config import configure_yookassa
