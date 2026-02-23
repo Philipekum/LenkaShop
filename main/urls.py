1fe78a3ac4fc813488e088d051b48e2fc77c6d08
Philipp <filipp.tsekhansky@yandex.ru>
2026-02-23 23:23:50 +0300

    temp redirect index->catalog

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
 from main.views import index, delivery_info, about_info
+from goods.views import catalog
 
 
 app_name = 'main'
 
 urlpatterns = [
-    path('', index, name='index'),
+    # path('', index, name='index'),
+    path('', catalog, name='index'),
     path('delivery-info/', delivery_info, name='delivery_info'),
     path('about-info/', about_info, name='about_info'),
 ]
