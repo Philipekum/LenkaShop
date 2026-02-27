761293802f52a9e48b529c07ed6e0e52ab9f312d
Philipp <filipp.tsekhansky@yandex.ru>
2026-02-27 16:49:20 +0300

    added telegram to admin panel

--- main/admin.py
+++ main/admin.py
@@ -5,7 +5,4 @@ from main.models import ContactInfo
 
 @admin.register(ContactInfo)
 class ContactInfoAdmin(admin.ModelAdmin):
-    list_display = ("phone", "email")
-
-    def has_add_permission(self, request):
-        return not ContactInfo.objects.exists()
+    list_display = ("phone", "email", "telegram")
